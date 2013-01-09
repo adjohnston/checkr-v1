6f660362b57157f09f56a9f13f84c7e1a49ccf12
adjohnston <adam.johnston3679@gmail.com>
2013-01-09 20:49:25 +0000

    added new methods, authentication

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,8 +1,22 @@
 class ListsController < ApplicationController
 
+  before_filter :authenticate_user!, only: [:new, :edit]
+  before_filter :find_list, only: [:edit, :update, :destroy]
+
+  def find_list
+    @list = current_user.lists.find(params[:id])
+  end
+
   def index
-    debugger
-    @lists = current_user ? current_user.lists : User.find_by_username(params[:username]).lists.find_by_is_public(true)
+    @lists = user_signed_in? ? current_user.lists : User.find_by_username(params[:username]).lists.find_all_by_is_public(true)
+  end
+
+  def show
+    @list = if user_signed_in? 
+      current_user.lists.find(params[:id]) 
+    else 
+      User.find_by_username(params[:username]).lists.find(params[:id])
+    end
   end
 
   def new
@@ -19,8 +33,18 @@ class ListsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @list.update_attributes(params[:list])
+      redirect_to lists_path(@list.id), notice: "#{@list.name} has been updated"
+    else
+      render :edit
+    end
+  end
+
   def destroy
-    @list = current_user.lists.find(:id)
     @list.delete
     redirect_to lists_path, notice: 'Your list has been deleted'
   end

b48656fffad2b217385612ff9f6d09559bb840d0
adjohnston <adam.johnston3679@gmail.com>
2013-01-10 19:53:09 +0000

    added item validations, associations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,7 @@
 class Item < ActiveRecord::Base
+  belongs_to :list
+
   attr_accessible :desc, :is_checked, :list_id, :name, :url
+
+  validates :name, presence: true
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,6 @@
 class List < ActiveRecord::Base
   belongs_to :user
+  has_many :items
 
   attr_accessible :desc, :is_public, :name, :user_id
 

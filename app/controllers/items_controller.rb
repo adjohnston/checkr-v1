09f93a78b0f9a9024e9c25b19795c25ad5fb1b03
adjohnston <adam.johnston3679@gmail.com>
2013-01-13 09:28:55 +0000

    added create and update methods

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,23 @@
 class ItemsController < ApplicationController
+
+  def create
+    @item = current_user.lists.find(params[:list_id]).items.new(params[:item])
+
+    if @item.save
+      redirect_to list_path(@item.list_id), notice: "#{@item.name} has been added"
+    else
+      render 'lists/show'
+    end
+  end
+
+  def update
+    @item = current_user.lists.find(params[:list_id]).items.find(params[:id])
+
+    if @item.update_attributes(params[:item])
+      redirect_to list_path(@item.list_id), notice: "#{@item.name} checked"
+    else
+      render 'lists/show'
+    end
+  end
+
 end

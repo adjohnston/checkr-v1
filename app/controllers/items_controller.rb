8ef8e5b16e908b5846f90282e8e7bf45219ed512
adjohnston <adam.johnston3679@gmail.com>
2013-01-17 19:56:49 +0000

    added edit, new and destroy methods

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,22 @@
 class ItemsController < ApplicationController
 
+  def new
+    @item = current_user.lists.find(params[:list_id]).items.new
+  end
+
   def create
     @item = current_user.lists.find(params[:list_id]).items.new(params[:item])
 
     if @item.save
       redirect_to list_path(@item.list_id), notice: "#{@item.name} has been added"
     else
-      render 'lists/show'
+      render :new
     end
+  end
+
+  def edit
+    @list = List.find(params[:list_id])
+    @item = @list.items.find(params[:id])
   end
 
   def update
@@ -16,7 +25,7 @@ class ItemsController < ApplicationController
     if @item.update_attributes(params[:item])
       redirect_to list_path(@item.list_id), notice: "#{@item.name} updated"
     else
-      render 'lists/show'
+      render :edit
     end
   end
 
@@ -30,6 +39,11 @@ class ItemsController < ApplicationController
       redirect_to user_signed_in? ? list_path(@item.list_id) : user_list_path(@username, @item.list_id), 
                   alert: 'There was an error, please try again later'
     end
+  end
+
+  def destroy
+    @item = current_user.lists.find(params[:list_id]).items.find(params[:id])
+    redirect_to list_item_path(@item.list_id, @item.id), notice: 'Your item was deleted' if @item.delete
   end
 
 end

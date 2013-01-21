0fb6bc13c400f1d288edec1f2d5aac73da435c18
adjohnston <adam.johnston3679@gmail.com>
2013-01-21 12:49:37 +0000

    added some html output to tie in with ajax functionality

--- app/helpers/lists_helper.rb
+++ app/helpers/lists_helper.rb
@@ -3,7 +3,7 @@ module ListsHelper
   protected
 
   def count_checked_items(list)
-    "#{list.items.select(&:is_checked).count} checked / #{list.items.count}"
+    raw("<span class=\"checked-items-count\">#{list.items.select(&:is_checked).count}</span> checked / #{list.items.count}")
   end
 
 end

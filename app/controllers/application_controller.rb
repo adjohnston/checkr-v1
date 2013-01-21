f66ac7bbb7639e34fd16652d8e9e3e03f614700d
adjohnston <adam.johnston3679@gmail.com>
2013-01-21 12:52:43 +0000

    added some helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  protected
+
+    def hyphenate(string)
+      string.gsub(' ', '-')
+    end
+
+    def spacify(string)
+      string.gsub('-', ' ')
+    end
+
+  helper_method :hyphenate, :spacify
 end

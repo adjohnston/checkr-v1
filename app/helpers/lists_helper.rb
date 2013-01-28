5cd6e9b9c6ad18d21fd4863f80f7ce4497a44229
adjohnston <adam.johnston3679@gmail.com>
2013-01-28 20:26:59 +0000

    added an extra class around total item count

--- app/helpers/lists_helper.rb
+++ app/helpers/lists_helper.rb
@@ -3,7 +3,7 @@ module ListsHelper
   protected
 
   def count_checked_items(list)
-    raw("<span class=\"checked-items-count\">#{list.items.select(&:is_checked).count}</span> checked / #{list.items.count}")
+    raw("<span class=\"checked-items-count\">#{list.items.select(&:is_checked).count}</span> checked / <span class=\"total-items-count\">#{list.items.count}</span>")
   end
 
 end
